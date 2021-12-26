0ce92af93027fe198d8ca3a68d810e41af044670
Argann BAY-EYMERE <argann.bay-eymere@epitech.eu>
2021-12-26 15:07:35 +0100

    + Added i18n Localization

--- src/App/App.js
+++ src/App/App.js
@@ -1,11 +1,21 @@
 import logo from '../logo.svg';
 import './App.css';
+import {useTranslation} from "react-i18next";
+import {changeLanguage} from "i18next";
 
 function App() {
+  const { t, i18n } = useTranslation();
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <button type="button" onClick={() => changeLanguage('fr')}>
+          FR
+        </button>
+        <button type="button" onClick={() => changeLanguage('en')}>
+          EN
+        </button>
+        <img src={logo} className="App-logo" alt="logo"/>
+        <p>{t('hello-world')}</p>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
